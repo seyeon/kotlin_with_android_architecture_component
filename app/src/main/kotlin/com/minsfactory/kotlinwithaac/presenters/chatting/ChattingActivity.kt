85176ee99c92a7a7329804b5c2099315f4db1bf4
seyeon <seyeon@futurewiz.co.kr>
2018-02-16 07:11:13 +0900

    add Chatting activity

---
+++ app/src/main/kotlin/com/minsfactory/kotlinwithaac/presenters/chatting/ChattingActivity.kt
@@ -0,0 +1,13 @@
+package com.minsfactory.kotlinwithaac.presenters.chatting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.minsfactory.kotlinwithaac.R
+
+class ChattingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting)
+    }
+}
