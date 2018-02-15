e4528fecb3192e87faef1ca72cd95e988e2c7896
seyeon <seyeon@futurewiz.co.kr>
2018-02-15 13:13:20 +0900

    Change the working path to kotlin.

--- app/src/androidTest/java/com/minsfactory/kotlinwithandroidarchitecturecomponent/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/minsfactory/kotlinwithandroidarchitecturecomponent/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.minsfactory.kotlinwithandroidarchitecturecomponent", appContext.packageName)
+        assertEquals("kotlinwithandroidarchitecturecomponent", appContext.packageName)
     }
 }
